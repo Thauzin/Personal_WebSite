099a2bdd28e49b194f66229e9b17abc39600c829
Thauzin | dev <thauanyfm3@gmail.com>
2024-05-12 11:08:01 -0300

    [ADD] - spotify

--- script.js
+++ script.js
@@ -7,3 +7,13 @@
   document.getElementById("volei").addEventListener("mouseout", function() {
       document.getElementById("textov").style.display = "none";
   });
+
+      // Função para mostrar o texto ao passar o mouse sobre a imagem
+      document.getElementById("spot").addEventListener("mouseover", function() {
+        document.getElementById("textos").style.display = "block";
+    });
+  
+    // Função para ocultar o texto ao retirar o mouse de cima da imagem
+    document.getElementById("spot").addEventListener("mouseout", function() {
+        document.getElementById("textos").style.display = "none"; 
+    });
