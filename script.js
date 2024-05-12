d0f9aaab6c45a69f3a1ce0aceca7c32d1a35604a
Thauzin | dev <thauanyfm3@gmail.com>
2024-05-12 19:58:36 -0300

    [ADD] - imagem e conf de tabuleiro

--- script.js
+++ script.js
@@ -8,12 +8,22 @@
       document.getElementById("textov").style.display = "none";
   });
 
-      // Função para mostrar o texto ao passar o mouse sobre a imagem
+      // Spotify
       document.getElementById("spot").addEventListener("mouseover", function() {
         document.getElementById("textos").style.display = "block";
     });
   
-    // Função para ocultar o texto ao retirar o mouse de cima da imagem
+    // Spotify
     document.getElementById("spot").addEventListener("mouseout", function() {
         document.getElementById("textos").style.display = "none"; 
     });
+
+    // Tabuleiro
+    document.getElementById("tabul").addEventListener("mouseover", function() {
+      document.getElementById("textot").style.display = "block";
+  });
+
+  // Tabuleiro
+  document.getElementById("tabul").addEventListener("mouseout", function() {
+      document.getElementById("textot").style.display = "none"; 
+  });
