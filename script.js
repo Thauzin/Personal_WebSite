26afc2fdd93cc48c469a9afec8e11ad20c47fb7e
Thauzin | dev <thauanyfm3@gmail.com>
2024-05-12 10:58:19 -0300

    [ADD] img c/ especial de JS

--- script.js
+++ script.js
@@ -1,23 +1,9 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var page1 = document.getElementById("page1");
-    var page2 = document.getElementById("page2");
-    
-    var currentPage = 1;
-    
-    document.addEventListener("mousemove", function(event) {
-      if (event.clientX > window.innerWidth / 2) {
-        if (currentPage !== 2) {
-          currentPage = 2;
-          page1.style.display = "none";
-          page2.style.display = "block";
-        }
-      } else {
-        if (currentPage !== 1) {
-          currentPage = 1;
-          page1.style.display = "block";
-          page2.style.display = "none";
-        }
-      }
-    });
+    // Função para mostrar o texto ao passar o mouse sobre a imagem
+    document.getElementById("volei").addEventListener("mouseover", function() {
+      document.getElementById("textov").style.display = "block";
   });
-  
+
+  // Função para ocultar o texto ao retirar o mouse de cima da imagem
+  document.getElementById("volei").addEventListener("mouseout", function() {
+      document.getElementById("textov").style.display = "none";
+  });
